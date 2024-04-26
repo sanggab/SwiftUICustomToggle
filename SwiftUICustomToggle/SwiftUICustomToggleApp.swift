48d377da0f8d86b50317ffd18db28a504487b601
심상갑 <kappa2410@gmail.com>
2024-04-26 09:08:25 +0900

    Initial Commit

---
+++ SwiftUICustomToggle/SwiftUICustomToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomToggleApp.swift
+//  SwiftUICustomToggle
+//
+//  Created by Gab on 2024/04/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
