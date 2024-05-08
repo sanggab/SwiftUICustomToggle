06c4ae2714f14d2d376d822d73d409e9f520e4c9
심상갑 <kappa2410@gmail.com>
2024-05-08 15:12:58 +0900

    Feat: UITest 해보는중

--- SwiftUICustomToggleUITests/SwiftUICustomToggleUITestsLaunchTests.swift
+++ SwiftUICustomToggleUITests/SwiftUICustomToggleUITestsLaunchTests.swift
@@ -19,14 +19,36 @@ final class SwiftUICustomToggleUITestsLaunchTests: XCTestCase {
 
     func testLaunch() throws {
         let app = XCUIApplication()
+        app.launchArguments = ["isRunningUITests"]
         app.launch()
-
+        
         // Insert steps here to perform after app launch but before taking a screenshot,
         // such as logging into a test account or navigating somewhere in the app
 
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
+//        let attachment = XCTAttachment(screenshot: app.screenshot())
+//        attachment.name = "Launch Screen"
+//        attachment.lifetime = .keepAlways
+//        add(attachment)
+    }
+    
+    func testToggleUI() {
+        let app = XCUIApplication()
+        app.launchArguments = ["UITest_MYToggle"]
+        app.launch()
+        
+//        let button = app.buttons["MyButton"]
+        let toggle = app.switches["MyToggle"]
+        
+//        XCTAssertNotNil(button)
+        XCTAssertNotNil(toggle)
+        
+//        XCTAssertTrue(button.label == "OFF", "button label : \(button.label)")
+        XCTAssertTrue(toggle.label == "OFF", "toggle label : \(toggle.label)")
+        
+//        button.tap()
+        toggle.switches.firstMatch.tap()
+        
+//        XCTAssertTrue(button.label == "ON", "button label : \(button.label)")
+        XCTAssertTrue(toggle.label == "ON", "toggle label : \(toggle.label)")
     }
 }
